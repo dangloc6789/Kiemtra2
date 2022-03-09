53acf59f3dfbfebf52a31590d8ba11180e72e503
Le Dat <letuandat314@gmail.com>
2022-03-09 14:47:53 +0700

    commit

--- Nhom_Canabis/Nhom_Canabis/Form1.Designer.cs
+++ Nhom_Canabis/Nhom_Canabis/Form1.Designer.cs
@@ -30,8 +30,8 @@ namespace Nhom_Canabis
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.textboxusername = new System.Windows.Forms.TextBox();
+            this.textboxmatkhau = new System.Windows.Forms.TextBox();
             this.dsf = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -39,57 +39,62 @@ namespace Nhom_Canabis
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 30);
+            this.label1.Location = new System.Drawing.Point(63, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(105, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên đăng nhập";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // txtUserName
+            // textboxusername
             // 
-            this.txtUserName.Location = new System.Drawing.Point(134, 25);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(163, 20);
-            this.txtUserName.TabIndex = 1;
+            this.textboxusername.Location = new System.Drawing.Point(179, 31);
+            this.textboxusername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxusername.Name = "textboxusername";
+            this.textboxusername.Size = new System.Drawing.Size(216, 22);
+            this.textboxusername.TabIndex = 1;
             // 
-            // txtPassword
+            // textboxmatkhau
             // 
-            this.txtPassword.Location = new System.Drawing.Point(437, 27);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(185, 20);
-            this.txtPassword.TabIndex = 3;
+            this.textboxmatkhau.Location = new System.Drawing.Point(583, 33);
+            this.textboxmatkhau.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxmatkhau.Name = "textboxmatkhau";
+            this.textboxmatkhau.Size = new System.Drawing.Size(245, 22);
+            this.textboxmatkhau.TabIndex = 3;
             // 
             // dsf
             // 
             this.dsf.AutoSize = true;
-            this.dsf.Location = new System.Drawing.Point(372, 32);
+            this.dsf.Location = new System.Drawing.Point(496, 39);
+            this.dsf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dsf.Name = "dsf";
-            this.dsf.Size = new System.Drawing.Size(52, 13);
+            this.dsf.Size = new System.Drawing.Size(66, 17);
             this.dsf.TabIndex = 2;
             this.dsf.Text = "Mật khẩu";
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(238, 77);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Location = new System.Drawing.Point(317, 95);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(193, 19);
+            this.btnLogin.Size = new System.Drawing.Size(257, 23);
             this.btnLogin.TabIndex = 4;
-            this.btnLogin.Text = "Login";
+            this.btnLogin.Text = "Đăng nhập";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.ClientSize = new System.Drawing.Size(931, 554);
             this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.textboxmatkhau);
             this.Controls.Add(this.dsf);
-            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.textboxusername);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -100,8 +105,8 @@ namespace Nhom_Canabis
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox textboxusername;
+        private System.Windows.Forms.TextBox textboxmatkhau;
         private System.Windows.Forms.Label dsf;
         private System.Windows.Forms.Button btnLogin;
     }
