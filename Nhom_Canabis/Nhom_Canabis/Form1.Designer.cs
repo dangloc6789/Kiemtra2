c8b1960feba5d46b6f82fbc7c30291296863680a
Le Dat <letuandat314@gmail.com>
2022-03-09 14:43:11 +0700

    commit

--- Nhom_Canabis/Nhom_Canabis/Form1.Designer.cs
+++ Nhom_Canabis/Nhom_Canabis/Form1.Designer.cs
@@ -31,51 +31,68 @@ namespace Nhom_Canabis
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
-            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
             this.dsf = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 30);
+            this.label1.Location = new System.Drawing.Point(63, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(75, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "UserName";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(112, 25);
+            this.txtUserName.Location = new System.Drawing.Point(149, 31);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(185, 20);
+            this.txtUserName.Size = new System.Drawing.Size(245, 22);
             this.txtUserName.TabIndex = 1;
             // 
-            // txtMatKhau
+            // txtPassword
             // 
-            this.txtMatKhau.Location = new System.Drawing.Point(437, 27);
-            this.txtMatKhau.Name = "txtMatKhau";
-            this.txtMatKhau.Size = new System.Drawing.Size(185, 20);
-            this.txtMatKhau.TabIndex = 3;
+            this.txtPassword.Location = new System.Drawing.Point(583, 33);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(245, 22);
+            this.txtPassword.TabIndex = 3;
             // 
             // dsf
             // 
             this.dsf.AutoSize = true;
-            this.dsf.Location = new System.Drawing.Point(372, 32);
+            this.dsf.Location = new System.Drawing.Point(496, 39);
+            this.dsf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dsf.Name = "dsf";
-            this.dsf.Size = new System.Drawing.Size(52, 13);
+            this.dsf.Size = new System.Drawing.Size(69, 17);
             this.dsf.TabIndex = 2;
-            this.dsf.Text = "Mật khẩu";
+            this.dsf.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(318, 95);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(257, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtMatKhau);
+            this.ClientSize = new System.Drawing.Size(931, 554);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.dsf);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -87,8 +104,9 @@ namespace Nhom_Canabis
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label dsf;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- Nhom_Canabis/Nhom_Canabis/Form1.cs
+++ Nhom_Canabis/Nhom_Canabis/Form1.cs
@@ -16,5 +16,10 @@ namespace Nhom_Canabis
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
