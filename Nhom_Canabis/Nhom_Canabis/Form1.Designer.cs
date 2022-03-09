24a97c941bfc123e43f406d63c1c68db634f6abd
dangloc6789 <83728109+dangloc6789@users.noreply.github.com>
2022-03-09 14:55:53 +0700

    commit last

--- Nhom_Canabis/Nhom_Canabis/Form1.Designer.cs
+++ Nhom_Canabis/Nhom_Canabis/Form1.Designer.cs
@@ -39,46 +39,42 @@ namespace Nhom_Canabis
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 37);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(47, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên đăng nhập";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textboxusername
             // 
-            this.textboxusername.Location = new System.Drawing.Point(179, 31);
-            this.textboxusername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxusername.Location = new System.Drawing.Point(134, 25);
             this.textboxusername.Name = "textboxusername";
-            this.textboxusername.Size = new System.Drawing.Size(216, 22);
+            this.textboxusername.Size = new System.Drawing.Size(163, 20);
             this.textboxusername.TabIndex = 1;
             // 
             // textboxmatkhau
             // 
-            this.textboxmatkhau.Location = new System.Drawing.Point(583, 33);
-            this.textboxmatkhau.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxmatkhau.Location = new System.Drawing.Point(437, 27);
             this.textboxmatkhau.Name = "textboxmatkhau";
-            this.textboxmatkhau.Size = new System.Drawing.Size(245, 22);
+            this.textboxmatkhau.Size = new System.Drawing.Size(185, 20);
             this.textboxmatkhau.TabIndex = 3;
             // 
             // dsf
             // 
             this.dsf.AutoSize = true;
-            this.dsf.Location = new System.Drawing.Point(496, 39);
-            this.dsf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dsf.Location = new System.Drawing.Point(372, 32);
             this.dsf.Name = "dsf";
-            this.dsf.Size = new System.Drawing.Size(66, 17);
+            this.dsf.Size = new System.Drawing.Size(52, 13);
             this.dsf.TabIndex = 2;
             this.dsf.Text = "Mật khẩu";
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(317, 95);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLogin.Location = new System.Drawing.Point(238, 77);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(257, 23);
+            this.btnLogin.Size = new System.Drawing.Size(193, 40);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Đăng nhập";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -86,15 +82,14 @@ namespace Nhom_Canabis
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(931, 554);
+            this.ClientSize = new System.Drawing.Size(698, 190);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textboxmatkhau);
             this.Controls.Add(this.dsf);
             this.Controls.Add(this.textboxusername);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Nhom_Canabis/Nhom_Canabis/Form1.cs
+++ Nhom_Canabis/Nhom_Canabis/Form1.cs
@@ -19,7 +19,11 @@ namespace Nhom_Canabis
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-
+            if(textboxmatkhau.Text== "admin" && textboxusername.Text== "admin")
+            {
+                Form2 f = new Form2();
+                f.Show();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
